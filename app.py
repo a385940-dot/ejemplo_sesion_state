bceb0092c7334b3a453312fabe751c6af5eb2f70
a385940-dot <a385940@uach.mx>
2025-10-29 09:05:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.title("Ejemplo para usar sesion_state")
 
 if 'count' not in st.sesion_state:
-  st.sesion_state["count"}= 0
+  st.sesion_state["count"]= 0
 
 st.write(st.sesion_state)
