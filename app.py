c47960cf9fd310b91c71c2d191a7de16651c53b6
a385940-dot <a385940@uach.mx>
2025-10-29 09:36:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ if "count" not in st.session_state:
 if "name" not in st.session_state:
   st.session_state["name"] = ""
 
-nombre=st.text_input("Escribe tu nombre", key="name"
+nombre=st.text_input("Escribe tu nombre", key="name")
 
 if st.button("Click me"):
   st.session_state["count"] +=1
