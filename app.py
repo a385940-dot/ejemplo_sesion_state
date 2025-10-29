6d918d36d196936efb8ba5737559837b282092bc
a385940-dot <a385940@uach.mx>
2025-10-29 09:03:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 import streamlit as st
 st.title("Ejemplo para usar sesion_state")
 
-count=0
+if"count" not in st.sesion_state:
+  st.sesion_state["count"}= 0
 
-increment = st.button("Increment")
-if increment:
-  count +=1
-
-st.write("count = " , count)
-
-st.write.sesion_state
+st.write(st.sesion_state)
